678ed5c1ccb4ec856b7f94751b640a06a5a6a25d
DGL <oliveir4.dg@gmail.com>
2022-08-25 20:29:54 -0300

    :pencil: Adicionado pasta do BootCamp Linux DIO

---
+++ DIO/Bootcamp Linux/script.sh
@@ -0,0 +1,39 @@
+echo "Criando usergroups."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando users."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Teste111) -G GRP_SEC
+
+echo "Criando pastas."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Alterando permissões."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Finalizado a execução do script"
